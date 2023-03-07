e5cf5eae0c37a4addb3a1b16bfbdda847289ee95
JongHoB <78012131+JongHoB@users.noreply.github.com>
2023-03-08 01:23:22 +0900

    [ADD] waht 테스트코드 작성 중

---
+++ test/waht.test.js
@@ -0,0 +1,284 @@
+const { execute } = require('../commands/waht');
+const axios = require('axios');
+jest.mock('axios');
+const log = require('../config/logger');
+
+describe('Testing Waht command', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+  test('waht check command works', async () => {
+    const interaction = {
+      commandName: 'waht',
+      options: {
+        getSubcommand: jest.fn().mockReturnValue('check'),
+      },
+      user: { id: '1234567890' },
+      reply: jest.fn(),
+    };
+    axios.mockReturnValue({
+      status: 200,
+    });
+    await execute(interaction);
+    expect(interaction.reply).toHaveBeenCalledWith({
+      content: `You are authenticated!`,
+      ephemeral: true,
+    });
+  });
+
+  test('waht events command not works because of unauthenticated', async () => {
+    const interaction = {
+      commandName: 'waht',
+      options: {
+        getSubcommand: jest.fn().mockReturnValue('events'),
+      },
+      user: { id: '1234567890' },
+      createdTimestamp: Date.now(),
+      deferReply: jest.fn(),
+      editReply: jest.fn(),
+    };
+
+    axios.mockImplementation(() =>
+      Promise.reject({ response: { status: 401 } })
+    );
+    await execute(interaction);
+    expect(interaction.deferReply).toHaveBeenCalledWith({ ephemeral: true });
+    expect(interaction.editReply).toHaveBeenCalledWith({
+      content: `Please authenticate your account`,
+    });
+  });
+
+  test('waht events command not works because of no smart contract address', async () => {
+    const interaction = {
+      commandName: 'waht',
+      options: {
+        getSubcommand: jest.fn().mockReturnValue('events'),
+      },
+      user: { id: '1234567890' },
+      createdTimestamp: Date.now(),
+      deferReply: jest.fn(),
+      editReply: jest.fn(),
+    };
+
+    axios.mockImplementation(() =>
+      Promise.reject({ response: { status: 404 } })
+    );
+    await execute(interaction);
+    expect(interaction.deferReply).toHaveBeenCalledWith({ ephemeral: true });
+    expect(interaction.editReply).toHaveBeenCalledWith({
+      content: `Couldn't find a list of events that match your smart contract address.`,
+    });
+  });
+
+  test('waht events command works', async () => {
+    const interaction = {
+      commandName: 'waht',
+      options: {
+        getSubcommand: jest.fn().mockReturnValue('events'),
+      },
+      user: { id: '1234567890' },
+      createdTimestamp: Date.now(),
+      deferReply: jest.fn(),
+      editReply: jest.fn(),
+    };
+
+    axios.mockResolvedValue({
+      status: 200,
+      data: {
+        data: [
+          {
+            start_time: '2023-03-08 00:00:00',
+            end_time: '2023-03-08 00:15:00',
+            event_name: 'test',
+            host_name: 'Test',
+            event_id: '12345',
+          },
+        ],
+      },
+    });
+
+    await execute(interaction);
+    expect(interaction.deferReply).toHaveBeenCalledWith({ ephemeral: true });
+    expect(interaction.editReply).toBeCalledTimes(1);
+  });
+
+  test('waht events command not works because of exceptional data status ', async () => {
+    const interaction = {
+      commandName: 'waht',
+      options: {
+        getSubcommand: jest.fn().mockReturnValue('events'),
+      },
+      user: { id: '1234567890' },
+      createdTimestamp: Date.now(),
+      deferReply: jest.fn(),
+      editReply: jest.fn(),
+    };
+
+    axios.mockResolvedValue({
+      status: 201,
+    });
+
+    await execute(interaction);
+    expect(interaction.deferReply).toHaveBeenCalledWith({ ephemeral: true });
+    expect(interaction.editReply).toHaveBeenCalledWith({
+      content: `You don't have any nfts.`,
+    });
+  });
+
+  test('waht refresh command  works', async () => {
+    const interaction = {
+      commandName: 'waht',
+      options: {
+        getSubcommand: jest.fn().mockReturnValue('refresh'),
+      },
+      user: { id: '1234567890' },
+      deferReply: jest.fn(),
+      editReply: jest.fn(),
+    };
+
+    axios.mockResolvedValue({
+      status: 200,
+    });
+    axios.patch.mockResolvedValue(true);
+    await execute(interaction);
+    expect(interaction.deferReply).toHaveBeenCalledWith({ ephemeral: true });
+    expect(interaction.editReply).toHaveBeenCalledWith({
+      content: 'Your NFT records have been updated',
+    });
+  });
+
+  test('waht refresh command not works because of unknown errors', async () => {
+    const interaction = {
+      commandName: 'waht',
+      options: {
+        getSubcommand: jest.fn().mockReturnValue('refresh'),
+      },
+      user: { id: '1234567890' },
+      deferReply: jest.fn(),
+      editReply: jest.fn(),
+    };
+
+    const mockLog = jest.spyOn(log, 'error').mockImplementation();
+
+    axios.mockResolvedValue({
+      status: 200,
+    });
+    axios.patch.mockRejectedValue(new Error('ERROR'));
+
+    await execute(interaction);
+    expect(interaction.deferReply).toHaveBeenCalledWith({ ephemeral: true });
+    expect(mockLog).toBeCalledTimes(1);
+    expect(interaction.editReply).toHaveBeenCalledWith({
+      content: `There was an error processing your request. Please try again later`,
+    });
+  });
+
+  test('waht refresh command not works because of unauthenticated', async () => {
+    const interaction = {
+      commandName: 'waht',
+      options: {
+        getSubcommand: jest.fn().mockReturnValue('refresh'),
+      },
+      user: { id: '1234567890' },
+      deferReply: jest.fn(),
+      editReply: jest.fn(),
+    };
+    axios.mockImplementation(() =>
+      Promise.reject({ response: { status: 401 } })
+    );
+    await execute(interaction);
+    expect(interaction.deferReply).toHaveBeenCalledWith({ ephemeral: true });
+    expect(interaction.editReply).toHaveBeenCalledWith({
+      content: `Please authenticate your account`,
+    });
+  });
+
+  test('waht rsvp command works', async () => {
+    const interaction = {
+      commandName: 'waht',
+      options: {
+        getSubcommand: jest.fn().mockReturnValue('rsvp'),
+      },
+      user: { id: '1234567890' },
+      deferReply: jest.fn(),
+      createdTimestamp: Date.now(),
+      editReply: jest.fn(),
+    };
+    axios.mockResolvedValue({
+      status: 200,
+      data: {
+        list: [
+          {
+            start_time: '2023-03-08 00:00:00',
+            end_time: '2023-03-08 00:15:00',
+            event_name: 'test',
+            host_name: 'Test',
+            event_id: '12345',
+          },
+        ],
+      },
+    });
+    await execute(interaction);
+    expect(interaction.deferReply).toHaveBeenCalledWith({ ephemeral: true });
+    expect(interaction.editReply).toBeCalledTimes(1);
+  });
+
+  test('waht rsvp command not works because no records', async () => {
+    const interaction = {
+      commandName: 'waht',
+      options: {
+        getSubcommand: jest.fn().mockReturnValue('rsvp'),
+      },
+      user: { id: '1234567890' },
+      deferReply: jest.fn(),
+      createdTimestamp: Date.now(),
+      editReply: jest.fn(),
+    };
+    axios.mockResolvedValue({
+      status: 204,
+    });
+    await execute(interaction);
+    expect(interaction.deferReply).toHaveBeenCalledWith({ ephemeral: true });
+    expect(interaction.editReply).toBeCalledTimes(1);
+  });
+
+  test('waht rsvp command not works because of unauthenticated', async () => {
+    const interaction = {
+      commandName: 'waht',
+      options: {
+        getSubcommand: jest.fn().mockReturnValue('rsvp'),
+      },
+      user: { id: '1234567890' },
+      deferReply: jest.fn(),
+      editReply: jest.fn(),
+    };
+    axios.mockImplementation(() =>
+      Promise.reject({ response: { status: 401 } })
+    );
+    await execute(interaction);
+    expect(interaction.deferReply).toHaveBeenCalledWith({ ephemeral: true });
+    expect(interaction.editReply).toHaveBeenCalledWith({
+      content: `Please authenticate your account`,
+    });
+  });
+
+  test('waht rsvp command not works because of no RSVP', async () => {
+    const interaction = {
+      commandName: 'waht',
+      options: {
+        getSubcommand: jest.fn().mockReturnValue('rsvp'),
+      },
+      user: { id: '1234567890' },
+      deferReply: jest.fn(),
+      editReply: jest.fn(),
+    };
+    axios.mockImplementation(() =>
+      Promise.reject({ response: { status: 404 } })
+    );
+    await execute(interaction);
+    expect(interaction.deferReply).toHaveBeenCalledWith({ ephemeral: true });
+    expect(interaction.editReply).toHaveBeenCalledWith({
+      content: `You haven't RSVP'd to any events.`,
+    });
+  });
+});
