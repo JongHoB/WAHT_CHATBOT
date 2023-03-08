b9442fd37624c6c54ecf499bad0892d2bc94d545
JongHoB <78012131+JongHoB@users.noreply.github.com>
2023-03-08 10:20:33 +0900

    [ADD] wallet connect 부분 작성 중

--- test/waht.test.js
+++ test/waht.test.js
@@ -1,6 +1,14 @@
+const WalletConnect = require('@walletconnect/client').default;
 const { execute } = require('../commands/waht');
+
+jest.mock('@walletconnect/client'.default);
+
+const { qrImageGenerator } = require('../utils/qrImageGenerator');
+// jest.mock('../utils/qrImageGenerator');
+
 const axios = require('axios');
 jest.mock('axios');
+
 const log = require('../config/logger');
 
 describe('Testing Waht command', () => {
@@ -279,6 +287,54 @@ describe('Testing Waht command', () => {
     expect(interaction.deferReply).toHaveBeenCalledWith({ ephemeral: true });
     expect(interaction.editReply).toHaveBeenCalledWith({
       content: `You haven't RSVP'd to any events.`,
+    });
+  });
+
+  describe('Testing Waht command', () => {
+    test('waht check command works', async () => {
+      const interaction = {
+        commandName: 'waht',
+        options: {
+          getSubcommand: jest.fn().mockReturnValue('check'),
+        },
+        user: { id: '1234567890' },
+        reply: jest.fn(),
+        deleteReply: jest.fn(),
+      };
+
+      const walletAddress = '0x123';
+      const client = {
+        createSession: jest.fn(),
+        sessionId: Math.floor(Math.random() * 1000000),
+        uri: 'walletconnect:',
+        on: jest.fn(),
+        sendCustomRequest: jest.fn(() => Promise.resolve([walletAddress])),
+        killSession: jest.fn(),
+      };
+
+      axios.mockImplementation(() =>
+        Promise.reject({ response: { status: 401 } })
+      );
+
+      await execute(interaction);
+
+      expect(interaction.reply).toHaveBeenCalledWith({
+        content: expect.any(String),
+        files: [{ attachment: expect.any(String), name: 'WalletConnect.png' }],
+        ephemeral: true,
+      });
+
+      expect(axios.post).toHaveBeenCalledWith(
+        `http://${process.env.SERVER_URL}/check?id=${interaction.user.id}`,
+        {
+          walletAddress: walletAddress,
+        }
+      );
+
+      expect(interaction.followUp).toHaveBeenCalledWith({
+        content: `You are now authenticated!`,
+        ephemeral: true,
+      });
     });
   });
 });
